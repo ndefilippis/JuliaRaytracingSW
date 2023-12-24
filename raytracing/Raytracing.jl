d491590ccc1e6635e4c28daf8ddc46e38d9cbe95
Nick DeFilippis <ndefilippis98@gmail.com>
2023-12-24 14:17:33 -0500

    Remove Ensemble

--- raytracing/Raytracing.jl
+++ raytracing/Raytracing.jl
@@ -31,19 +31,19 @@ struct VelocityGradientInterpolator
     vy::AbstractInterpolation
 end
 
-function createVelocityInterpolator(U::Velocity, x, y)
+function createVelocityInterpolator(U::Velocity, x::AbstractRange{Float64}, y::AbstractRange{Float64})
     u_itp = interpolator(U.u, x, y);
     v_itp = interpolator(U.v, x, y);
     return VelocityInterpolator(u_itp, v_itp);
 end
 
-function createVelocityInterpolator(U1::Velocity, U2::Velocity, x, y, tspan)
+function createVelocityInterpolator(U1::Velocity, U2::Velocity, x::AbstractRange{Float64}, y::AbstractRange{Float64}, tspan)
     u_itp = interpolator(U1.u, U2.u, x, y, tspan);
     v_itp = interpolator(U1.v, U2.v, x, y, tspan);
     return VelocityInterpolator(u_itp, v_itp);
 end
 
-function createVelocityGradientInterpolator(dU::VelocityGradient, x, y)
+function createVelocityGradientInterpolator(dU::VelocityGradient, x::AbstractRange{Float64}, y::AbstractRange{Float64})
     ux_itp = interpolator(dU.ux, x, y);
     uy_itp = interpolator(dU.uy, x, y);
     vx_itp = interpolator(dU.vx, x, y);
@@ -51,7 +51,7 @@ function createVelocityGradientInterpolator(dU::VelocityGradient, x, y)
     return VelocityGradientInterpolator(ux_itp, uy_itp, vx_itp, vy_itp);
 end
 
-function createVelocityGradientInterpolator(dU1::VelocityGradient, dU2::VelocityGradient, x, y, tspan)
+function createVelocityGradientInterpolator(dU1::VelocityGradient, dU2::VelocityGradient, x::AbstractRange{Float64}, y::AbstractRange{Float64}, tspan)
     ux_itp = interpolator(dU1.ux, dU2.ux, x, y, tspan);
     uy_itp = interpolator(dU1.uy, dU2.uy, x, y, tspan);
     vx_itp = interpolator(dU1.vx, dU2.vx, x, y, tspan);
@@ -59,25 +59,29 @@ function createVelocityGradientInterpolator(dU1::VelocityGradient, dU2::Velocity
     return VelocityGradientInterpolator(ux_itp, uy_itp, vx_itp, vy_itp);
 end
 
+@nospecialize
 function dispersion_relation(k, params)
     return sqrt(params.f^2 + params.Cg^2*(k[1]*k[1] + k[2]*k[2]));
 end
 
+@nospecialize
 function group_velocity(k, params)
    return params.Cg^2*k/dispersion_relation(k, params); 
 end
 
+@nospecialize
 function hamiltonian(x, k, params, t)
    return params.u(x[1], x[2], t)*k[1] + params.v(x[1], x[2], t) * k[2] + dispersion_relation(k, params)
 end
 
-
+@nospecialize
 function dxdt(xdot, x, k, params, t)
     group_vel = group_velocity(k, params);
     xdot[1] = params.u(x[1], x[2], t) + group_vel[1];
     xdot[2] = params.v(x[1], x[2], t) + group_vel[2];
 end
 
+@nospecialize
 function dkdt(kdot, x, k, params, t);
     kdot[1] = -params.ux(x[1], x[2], t)*k[1] - params.vx(x[1], x[2], t)*k[2];
     kdot[2] = -params.uy(x[1], x[2], t)*k[1] - params.vy(x[1], x[2], t)*k[2];
@@ -91,17 +95,22 @@ function _solve!(Npackets::Int, wavepackets::AbstractVector{Wavepacket}, dt::Flo
     function prob_func(prob, i, repeat)
         remake(prob, u0 = ics[i]);
     end
-	output_func(sol, i) = (sol[end], false)
-    problem = DynamicalODEProblem(dxdt, dkdt, ics[1][1:2], ics[1][3:4], tspan, params);
-    ensemble_prob = EnsembleProblem(problem, prob_func = prob_func, output_func = output_func, safetycopy=false);
-    sim = solve(ensemble_prob, ImplicitMidpoint(), EnsembleThreads(), trajectories=Npackets, dt=dt, save_on=false, save_start=false)
-    for i=1:Npackets;
-        wavepackets[i] = Wavepacket(sim[1:2,i], sim[3:4,i]);
+	#output_func(sol, i) = (sol[end], false)
+    println("Create problem:")
+    #@time problem = DynamicalODEProblem(dxdt, dkdt, ics[1][1:2], ics[1][3:4], tspan, params);
+    #println("Create ensemble:")
+    #@time ensemble_prob = EnsembleProblem(problem, prob_func = prob_func, output_func = output_func, safetycopy=false);
+    #println("Solve:")
+    #@time sim = solve(ensemble_prob, ImplicitMidpoint(), EnsembleThreads(), trajectories=Npackets, dt=dt, save_on=false, save_start=false)
+    Threads.@threads for i=1:Npackets;
+        problem = DynamicalODEProblem(dxdt, dkdt, ics[i][1:2], ics[i][3:4], tspan, params);
+        @time sim = solve(problem, ImplicitMidpoint(), dt=dt, save_on=false, save_start=false);
+        wavepackets[i] = Wavepacket(sim[1:2,end], sim[3:4,end]);
     end
     return wavepackets;
 end
 
-function solve!(velocity::Velocity, gradient::VelocityGradient, x, y, Npackets::Int, wavepackets::AbstractVector{Wavepacket}, dt::Float64, tspan::Tuple{Float64, Float64}, params)
+function solve!(velocity::Velocity, gradient::VelocityGradient, x::AbstractRange{Float64}, y::AbstractRange{Float64}, Npackets::Int, wavepackets::AbstractVector{Wavepacket}, dt::Float64, tspan::Tuple{Float64, Float64}, params)
 
     velocityInterpolator = createVelocityInterpolator(velocity, x, y);
     velocityGradientInterpolator = createVelocityGradientInterpolator(gradient, x, y);
@@ -118,7 +127,7 @@ function solve!(velocity::Velocity, gradient::VelocityGradient, x, y, Npackets::
 end
 
 function solve!(velocity1::Velocity, velocity2::Velocity, gradient1::VelocityGradient, gradient2::VelocityGradient, 
-    x, y, Npackets::Int, wavepackets::AbstractVector{Wavepacket}, dt::Float64, tspan::Tuple{Float64, Float64}, params)
+    x::AbstractRange{Float64}, y::AbstractRange{Float64}, Npackets::Int, wavepackets::AbstractVector{Wavepacket}, dt::Float64, tspan::Tuple{Float64, Float64}, params)
 
     velocityInterpolator = createVelocityInterpolator(velocity1, velocity2, x, y, tspan);
     velocityGradientInterpolator = createVelocityGradientInterpolator(gradient1, gradient2, x, y, tspan);
@@ -142,7 +151,7 @@ function interpolator(field, x, y)
         Periodic());
 end
 
-function interpolator(field1, field2, x, y, tspan)
+function interpolator(field1::Array{Float64, 2}, field2::Array{Float64, 2}, x::AbstractRange{Float64}, y::AbstractRange{Float64}, tspan)
     # return cubic_spline_interpolation((grid.x, grid.y), field, extrapolation_bc = Periodic());
     field = cat(field1, field2, dims=3);
     return extrapolate(
