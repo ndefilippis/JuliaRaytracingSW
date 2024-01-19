fbbe6ee853526866840612bba698c22a8525ca82
Nick DeFilippis <nad9961@log-2.hpc.nyu.edu>
2024-01-19 11:10:20 -0500

    Uncomment raytracing

--- raytracing/Parameters.jl
+++ raytracing/Parameters.jl
@@ -4,7 +4,7 @@ module Parameters
 
 # Integrator parameters
 stepper = "FilteredETDRK4"
-nsteps = 100000
+nsteps = 10000
 #nx = 512 # number of grid points
 
 # Domain parameters
@@ -13,7 +13,7 @@ L = 2π                   # domain size
 # Output parameters
 filepath = "."
 filename = "packets.jld2"
-nsubs = 1;
+nsubs = 10;
 npacketsubs = 50;
 
 initial_condition_file = "initial_condition.jld2"
@@ -22,7 +22,7 @@ initial_condition_file = "initial_condition.jld2"
 packetSpinUpDelay = 0; # Timesteps until we start advecting wavepackets
 sqrtNpackets = 10; # Square root of the number of wavepackets;
 Npackets = sqrtNpackets^2;
-initialFroudeNumber = 1e-1; # Scale initial steady background field to achieve this Froude number
+initialFroudeNumber = 3e-1; # Scale initial steady background field to achieve this Froude number
 corFactor = 2.; # How close to f the initial wavepacket frequencies are
 packetStepsPerBackgroundStep = 10
 end
