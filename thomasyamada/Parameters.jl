a7704d5cc80840e70d1cf6892bc1b1c2ff4e6d68
Nick DeFilippis <ndefilippis98@gmail.com>
2024-01-25 14:36:20 -0500

    Allow for runs on the GPU

--- thomasyamada/Parameters.jl
+++ thomasyamada/Parameters.jl
@@ -1,20 +1,22 @@
 module Parameters
     # Simulation parameters
-    nx = 384
-    Ro = 0.3 # Rossby number
-    ν  = 2.4e-34
+    nx = 128
+    Ro = 0.2 # Rossby number
+    ν  = 3.5e-25
     nν = 8
 
     # Time stepping parameters
+    device = "GPU"
     dt = 5e-2
     stepper = "ETDRK4"
 
     # Output parameters
-    nsteps = 10000
+    nsteps = 30000
     nsubs  = 50    # Number of timesteps before outputting
     filename = "ty.jld2"
     
     # Initial condition parameters
+    k0 = 6
     Et = 0.3 # Initial barotropic energy
     Eg = Et  # Initial baroclinic geostrophic balanced energy
     Ew = 0.3 # Initial baroclinic inertia-gravity wave energy
