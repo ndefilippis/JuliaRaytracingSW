446d50c5a90abe9a8788a58b3b7d18ced2fc44af
Nick DeFilippis <ndefilippis98@gmail.com>
2024-01-25 18:09:30 -0500

    Decouple plotting and simulation

--- thomasyamada/TYdriver.jl
+++ thomasyamada/TYdriver.jl
@@ -113,6 +113,24 @@ function set_initial_condition(prob; k0=0, Et=0.0, Eg=0.0, Ew=0.0)
     set_solution!(prob, ζ₀h, u₀h, v₀h, p₀h)
 end
 
+function create_figure(ζt, qc, sol, baroclinic, barotropic)
+    fig = Figure(size=(1200,400))
+    axζ = Axis(fig[1,1][1,1]; title="ζ_T")
+    axq = Axis(fig[1,2][1,1]; title="q_C")
+    axE = Axis(fig[1,3][1,1], xlabel="t", ylabel="bc E", limits=((0, nsteps * prob.clock.dt), (0, 10.)))
+
+    ζhm = heatmap!(axζ, x, y, ζt; colormap = :balance)
+    qhm = heatmap!(axq, x, y, qc; colormap = :balance)
+
+    Colorbar(fig[1,1][2, 1], ζhm, vertical=false, flipaxis = false)
+    Colorbar(fig[1,2][2, 1], qhm, vertical=false, flipaxis = false)
+    bc = lines!(axE, baroclinic, label="bc E"; linewidth = 3)
+    bt = lines!(axE, barotropic, label="bt E"; linewidth = 3)
+    axislegend(axE) 
+    
+    return fig
+end
+
 function start!()
     nsteps = Parameters.nsteps
     nsubs = Parameters.nsubs
@@ -145,33 +163,21 @@ function start!()
     btE = Diagnostic(barotropic_energy, prob; nsteps)
     diags = [bcE, btE]
 
-    
     ζt = Observable(Array(vars.ζt))
     qc = Observable(Array(vars.qc))
+    solution = Observable(Array(sol))
     baroclinic = Observable(Point2f[(bcE.t[1], bcE.data[1])])
     barotropic = Observable(Point2f[(btE.t[1], btE.data[1])])
 
-    fig = Figure(size=(1200,400))
-    axζ = Axis(fig[1,1][1,1]; title="ζ_T")
-    axq = Axis(fig[1,2][1,1]; title="q_C")
-    axE = Axis(fig[1,3][1,1], xlabel="t", ylabel="bc E", limits=((0, nsteps * prob.clock.dt), (0, 10.)))
-
-    ζhm = heatmap!(axζ, x, y, ζt; colormap = :balance)
-    qhm = heatmap!(axq, x, y, qc; colormap = :balance)
-
-    Colorbar(fig[1,1][2, 1], ζhm, vertical=false, flipaxis = false)
-    Colorbar(fig[1,2][2, 1], qhm, vertical=false, flipaxis = false)
-    bc = lines!(axE, baroclinic, label="bc E"; linewidth = 3)
-    bt = lines!(axE, barotropic, label="bt E"; linewidth = 3)
-    axislegend(axE)
+    # fig = create_figure(ζt, qc, sol, baroclinic, barotropic)
 
     startwalltime = time()
     frames = 0:round(Int, nsteps / nsubs)
 
     updatevars!(prob)
     saveproblem(out)
-  # for j=frames
-    record(fig, "thomas_yamada.mp4", frames, framerate = 18) do j
+    for j=frames
+        #record(fig, "thomas_yamada.mp4", frames, framerate = 18) do j
         if j % (1000 / nsubs) == 0
             max_udx = maximum([maximum(vars.uc) / grid.dx, maximum(vars.vc) / grid.dy, maximum(vars.ut) / grid.dx, maximum(vars.vt) / grid.dy])
             cfl = clock.dt * max_udx
@@ -179,10 +185,11 @@ function start!()
             println(log)
 			flush(stdout)	
         end
-        ζt[] = vars.ζt
-        qc[] = vars.qc
-        baroclinic[] = push!(baroclinic[], Point2f(bcE.t[bcE.i], bcE.data[bcE.i][1]))
-        barotropic[] = push!(barotropic[], Point2f(btE.t[btE.i], btE.data[btE.i][1]))
+        #ζt[] = vars.ζt
+        #qc[] = vars.qc
+        #baroclinic[] = push!(baroclinic[], Point2f(bcE.t[bcE.i], bcE.data[bcE.i][1]))
+        #barotropic[] = push!(barotropic[], Point2f(btE.t[btE.i], btE.data[btE.i][1]))
+        #solution[] = sol
         stepforward!(prob, diags, nsubs)
         updatevars!(prob)
         saveoutput(out)
